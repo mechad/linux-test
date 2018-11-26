ceb7c4bdda31579070b7a76b55f26792f7637f98
lin, chaochao <chaochaox.lin@intel.com>
2018-11-26 17:06:03 +0800

    web test

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ********************************************************
+# @file: /var/www/cgi-bin/hello.sh
+# @author: Lin, Chao <chaochaox.lin@intel.com>
+# @create time: 2018-11-26 14:32:20
+# @last modified: 2018-11-26 14:32:20
+# @description:
+# ********************************************************
+
+echo "Content-Type:text/html"
+echo "" 
+echo '{"studentID":"123456","studentName":"张三","class":"1般","department":"121","teleNumber":"1234567890"}|'
+echo '{"studentID":"123456","studentName":"张三","class":"1般","department":"121","teleNumber":"1234567890"}|'
+echo '{"studentID":"123456","studentName":"张三","class":"1般","department":"121","teleNumber":"1234567890"}'
